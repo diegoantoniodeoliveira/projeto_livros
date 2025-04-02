908f78545d8c45a34f6c30a8285db510415099ac
diegoantoniodeoliveira <diegoantoniodeoliveira@hotmail.com>
2025-04-02 10:37:02 -0300

    scss ajustado

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -31,7 +31,7 @@ export default function Doados(){
 
                 {livros.map((item)=>(
                     <article key={item.id}>
-                        <img src={item.imagem_url} alt="" />
+                        <img src={item.imagem_url} alt={`Imagem do livro: ${item.titulo}`} />  
                         <h3>{item.titulo}</h3>
                         <p>{item.categoria}</p>
                         <p>{item.autor}</p>
